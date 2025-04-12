5b24d3c14bfde5f579cfa13d06bfab1e86093527
Anusri Kadam <anusrikadam@gmail.com>
2025-04-12 15:11:03 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,7 @@
+// Subtraction.java
+public class Subtraction {
+    // This method performs subtraction of two numbers
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+}
