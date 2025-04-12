eb118deae5824fac973512c9c3417f149dbaa96d
Anusri Kadam <anusrikadam@gmail.com>
2025-04-12 15:11:28 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,7 @@
+// Multiplication.java
+public class Multiplication {
+    // This method performs multiplication of two numbers
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+}
