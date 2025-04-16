6c426825b65f21606f1737990f877fbe470fbb40
Anusri Kadam <anusrikadam@gmail.com>
2025-04-16 09:08:36 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,9 @@
+// Division.java
+public class Division {
+    public static double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
